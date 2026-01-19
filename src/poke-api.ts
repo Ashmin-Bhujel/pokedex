9ad899514b64c2107a6337e86d4f8f63be72c374
Ashmin Bhujel <ashminbhujel01@gmail.com>
2026-01-19 18:56:23 +0545

    feat: fetch location method

--- src/poke-api.ts
+++ src/poke-api.ts
@@ -35,6 +35,32 @@ export class PokeAPI {
       throw new Error(`Error fetching locations: ${(e as Error).message}`);
     }
   }
+
+  async fetchLocation(locationName: string): Promise<Location> {
+    const url = `${PokeAPI.baseURL}/location-area/${locationName}`;
+    const cached = this.cache.get<Location>(url);
+    if (cached) {
+      console.log("=========");
+      console.log("Getting cached data");
+      return cached;
+    }
+
+    try {
+      const resp = await fetch(url);
+
+      if (!resp.ok) {
+        throw new Error(`${resp.status} ${resp.statusText}`);
+      }
+
+      const location: Location = await resp.json();
+      this.cache.add(url, location);
+      return location;
+    } catch (e) {
+      throw new Error(
+        `Error fetching location '${locationName}': ${(e as Error).message}`,
+      );
+    }
+  }
 }
 
 export type ShallowLocations = {
@@ -44,5 +70,58 @@ export type ShallowLocations = {
   results: {
     name: string;
     url: string;
+  }[];
+};
+
+export type Location = {
+  encounter_method_rates: {
+    encounter_method: {
+      name: string;
+      url: string;
+    };
+    version_details: {
+      rate: number;
+      version: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
+  game_index: number;
+  id: number;
+  location: {
+    name: string;
+    url: string;
+  };
+  name: string;
+  names: {
+    language: {
+      name: string;
+      url: string;
+    };
+    name: string;
+  }[];
+  pokemon_encounters: {
+    pokemon: {
+      name: string;
+      url: string;
+    };
+    version_details: {
+      encounter_details: {
+        chance: number;
+        condition_values: any[];
+        max_level: number;
+        method: {
+          name: string;
+          url: string;
+        };
+        min_level: number;
+      }[];
+      max_chance: number;
+      version: {
+        name: string;
+        url: string;
+      };
+    }[];
   }[];
 };
