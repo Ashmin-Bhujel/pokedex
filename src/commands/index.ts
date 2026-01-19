ecf9b64c128279a74f5f9829f2f9be5cfde81860
Ashmin Bhujel <ashminbhujel01@gmail.com>
2026-01-19 20:15:11 +0545

    feat: inspect command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,6 +3,7 @@ import { commandCatch } from "./command-catch.js";
 import { commandExit } from "./command-exit.js";
 import { commandExplore } from "./command-explore.js";
 import { commandHelp } from "./command-help.js";
+import { commandInspect } from "./command-inspect.js";
 import { commandMapBack, commandMapForward } from "./command-map.js";
 
 export function getCommands(): Record<string, CLICommand> {
@@ -36,6 +37,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "catch <pokemon_name>",
       description: "Attempt to catch a pokemon",
       callback: commandCatch,
+    },
+    inspect: {
+      name: "inspect <pokemon_name>",
+      description: "View details about a caught pokemon",
+      callback: commandInspect,
     },
   };
 }
